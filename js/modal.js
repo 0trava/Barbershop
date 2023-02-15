7091d9023e0bca6b306afa28282b14e6f3bc44c5
Svetlana Lytvyn <102797527+0trava@users.noreply.github.com>
2023-02-15 22:18:29 +0200

    modal window

--- js/modal.js
+++ js/modal.js
@@ -1,11 +1,20 @@
+
+
+
 (() => {
+
+
     const refs = {
       openModalBtn: document.querySelector("[data-modal-open]"),
+      openModalBtn1: document.querySelector("[data-modal-open1]"),
+      openModalBtn2: document.querySelector("[data-modal-open2]"),
       closeModalBtn: document.querySelector("[data-modal-close]"),
       modal: document.querySelector("[data-modal]"),
     };
   
     refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.openModalBtn1.addEventListener("click", toggleModal);
+    refs.openModalBtn2.addEventListener("click", toggleModal);
     refs.closeModalBtn.addEventListener("click", toggleModal);
   
     function toggleModal() {
