6765d0e2efdf3020ad75860fb00c40ffd67a57ad
Svetlana Lytvyn <102797527+0trava@users.noreply.github.com>
2023-03-26 14:44:37 +0300

    fix btn bug

--- js/menu-mob.js
+++ js/menu-mob.js
@@ -3,11 +3,13 @@
     const refs = {
       openModalBtn: document.querySelector("[data-mob-open]"),
       closeModalBtn: document.querySelector("[data-mob-close]"),
+      closeModalBtn3: document.querySelector("[data-mob-close3]"),
       modal: document.querySelector("[data-modal-mob]"),
     };
   
     refs.openModalBtn.addEventListener("click", toggleModal);
     refs.closeModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn3.addEventListener("click", toggleModal);
   
     function toggleModal() {
       refs.modal.classList.toggle("is-hidden");
--- js/modal.js
+++ js/modal.js
@@ -8,6 +8,7 @@
       openModalBtn: document.querySelector("[data-modal-open]"),
       openModalBtn1: document.querySelector("[data-modal-open1]"),
       openModalBtn2: document.querySelector("[data-modal-open2]"),
+      openModalBtn3: document.querySelector("[data-modal-open3]"),
       closeModalBtn: document.querySelector("[data-modal-close]"),
       modal: document.querySelector("[data-modal]"),
     };
@@ -15,6 +16,7 @@
     refs.openModalBtn.addEventListener("click", toggleModal);
     refs.openModalBtn1.addEventListener("click", toggleModal);
     refs.openModalBtn2.addEventListener("click", toggleModal);
+    refs.openModalBtn3.addEventListener("click", toggleModal);
     refs.closeModalBtn.addEventListener("click", toggleModal);
   
     function toggleModal() {
